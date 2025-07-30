72327f767a167036ed9415cb8a9886e5fffbf959
Asus <Asus@LAPTOP-JSLOGT9H>
2025-07-30 11:43:59 +0530

    new line added

--- shopping_app/src/entity/SampleEntity.java
+++ shopping_app/src/entity/SampleEntity.java
@@ -5,6 +5,7 @@ public class SampleEntity {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Welcome....");
+System.out.println("uploading...");
 	}
 
 }
